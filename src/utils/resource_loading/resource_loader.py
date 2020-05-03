1eee68291991bcbb1ff6a548a87508eb268733ea
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2020-05-03 10:59:59 -0300

    CAST-103 -- created file but need client confirmation to create file reader

---
+++ src/utils/resource_loading/resource_loader.py
@@ -0,0 +1,7 @@
+class ResourceLoader:
+
+    @classmethod
+    def load_resources(cls):
+        """ Load resources to database for every non-existent collection. """
+        # TODO -> Need client confirmation on file format
+        pass
