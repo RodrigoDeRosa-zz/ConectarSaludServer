c9c8328a550f7a5c7f21ff1cd0945a9e656b5cac
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2020-05-04 20:54:32 -0300

    CST-103 -- basic user creation done

--- src/database/daos/authentication_dao.py
+++ src/database/daos/authentication_dao.py
@@ -13,12 +13,25 @@ class AuthenticationDAO(GenericDAO):
         return None if not document else cls.__to_object(document)
 
     @classmethod
+    async def add(cls, auth_data: AuthData):
+        """ Should only be used by resource loader """
+        await cls.insert(cls.__to_document(auth_data))
+
+    @classmethod
     def __to_object(cls, document: dict) -> AuthData:
         return AuthData(
             user_id=document['_id'],
             password=document['password'],
             role=document['role']
         )
+
+    @classmethod
+    def __to_document(cls, auth_data: AuthData) -> dict:
+        return {
+            '_id': auth_data.user_id,
+            'password': auth_data.password,
+            'role': auth_data.role
+        }
 
     @classmethod
     def collection(cls):
--- src/utils/resource_loading/resource_loader.py
+++ src/utils/resource_loading/resource_loader.py
@@ -3,7 +3,9 @@ from os.path import abspath, join, dirname
 
 from tornado.ioloop import IOLoop
 
+from src.database.daos.authentication_dao import AuthenticationDAO
 from src.database.daos.doctor_dao import DoctorDAO
+from src.model.authentication.auth_data import AuthData
 from src.service.resource_management.doctors.doctor_management_service import DoctorManagementService
 from src.service.resource_management.doctors.mappers.doctor_management_request_mapper import \
     DoctorManagementRequestMapper
@@ -13,11 +15,26 @@ from src.utils.logging.logger import Logger
 class ResourceLoader:
 
     DOCTORS = 'doctors.json'
+    USERS = 'users.json'
 
     @classmethod
     def load_resources(cls):
         """ Load resources to database for every non-existent collection. """
         IOLoop.current().add_timeout(0, cls.__load_doctors)
+        IOLoop.current().add_timeout(0, cls.__load_users)
+
+    @classmethod
+    async def __load_users(cls):
+        """ Read users file and load on database if it was not previously done. """
+        if await AuthenticationDAO.get_all({}): return
+        # Only load file if there are no users in the database
+        Logger(cls.__name__).info('Creating basic authentication database entries...')
+        file_name = f'{abspath(join(dirname(__file__), "../../.."))}/resources/initial_resources/{cls.USERS}'
+        with open(file_name) as fd:
+            users = load(fd)
+        # Add every doctor to the database
+        for user in users:
+            await AuthenticationDAO.add(AuthData(user_id=user['user_id'], password=user['password'], role=user['role']))
 
     @classmethod
     async def __load_doctors(cls):
