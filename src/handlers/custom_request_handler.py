a03c8f6f543235ece5e16cf6e44f9a4954c9c8e7
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2020-05-04 21:04:20 -0300

    added allow header to handler

--- src/handlers/custom_request_handler.py
+++ src/handlers/custom_request_handler.py
@@ -30,6 +30,7 @@ class CustomRequestHandler(RequestHandler):
         # Set default JSON header
         self.set_header('Content-Type', 'application/json')
         self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Allow', 'OPTIONS, GET, HEAD, POST, PATCH, PUT')
         # There are cases with no body
         if response:
             # The following is done to accept List responses (Tornado doesn't accept them by default)
--- src/handlers/resource_management/doctor_management_handler.py
+++ src/handlers/resource_management/doctor_management_handler.py
@@ -9,6 +9,8 @@ from src.service.resource_management.doctors.mappers.doctor_management_response_
 
 class DoctorManagementHandler(CustomRequestHandler):
 
+    SUPPORTED_METHODS = ['OPTIONS', 'GET', 'HEAD', 'POST', 'PATCH', 'PUT']
+
     async def post(self, doctor_id):
         """ Doctor resource creation endpoint. """
         try:
