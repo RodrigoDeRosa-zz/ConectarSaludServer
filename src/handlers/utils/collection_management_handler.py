6f143992d64e414258d3bbb43e69ba3244d0ea46
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2020-05-30 17:36:35 -0300

    added utility endpoint to clear collections

---
+++ src/handlers/utils/collection_management_handler.py
@@ -0,0 +1,14 @@
+from src.database.mongo import Mongo
+from src.handlers.custom_request_handler import CustomRequestHandler
+
+
+class CollectionCleaningHandler(CustomRequestHandler):
+
+    async def post(self, collection_name):
+        await self.wrap_handling(self.__clean_collection, **{'collection_name': collection_name})
+
+    """ Handling methods """
+
+    @staticmethod
+    async def __clean_collection(collection_name: str):
+        Mongo.get()[collection_name].remove({})
