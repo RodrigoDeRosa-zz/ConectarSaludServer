b610584caf4a5d4e4938fa58e0439fef12b140db
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2020-07-12 15:00:31 -0300

    made query params not compulsory

--- src/handlers/statistics/score_statistics_handler.py
+++ src/handlers/statistics/score_statistics_handler.py
@@ -14,13 +14,13 @@ class ScoreStatisticsHandler(CustomRequestHandler):
     async def __score_statistics(self):
         """ Creates a new consultation for the given affiliate. """
         # Parse query params
-        doctor_id = self.get_argument('doctor_id')
-        from_date = self.get_argument('from_date')
+        doctor_id = self.get_argument('doctor_id', None)
+        from_date = self.get_argument('from_date', None)
         # If there is no from_date, we get all consultations
         from_date = datetime.min if not from_date else datetime.strptime(from_date, '%d-%m-%Y')
-        to_date = self.get_argument('to_date')
+        to_date = self.get_argument('to_date', None)
         to_date = None if not to_date else datetime.strptime(from_date, '%d-%m-%Y')
-        specialty = self.get_argument('specialty')
+        specialty = self.get_argument('specialty', None)
         # Retrieve statistics
         score_list, detail = await ScoreStatisticsService.get_statistics(doctor_id, from_date, to_date, specialty)
         self.make_response({'average_by_date': score_list, 'scoring_data': detail})
